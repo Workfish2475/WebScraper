d112871dd62d2c17e7e2d5e176a955c14ada4d53
Alex <78886791+alexander2475@users.noreply.github.com>
2025-03-17 16:10:42 -0500

    updating to make use of static html landing page

--- jsonParse.py
+++ jsonParse.py
@@ -1,6 +1,4 @@
 import json
-from operator import contains
-
 
 class DataFetcher:
     def __init__(self):
@@ -10,64 +8,29 @@ class DataFetcher:
         try:
             with open(json_file, 'r') as file:
                 self.data = json.load(file)
-            return
         except FileNotFoundError:
             raise FileNotFoundError(f"File {json_file} not found")
         except json.JSONDecodeError:
             raise json.JSONDecodeError("Invalid JSON format")
 
+    def _filter_by_field(self, field, value):
+        if self.data is None:
+            return None
+
+        value = value.lower()
+        return [item for item in self.data if value in (item.get(field, "")).lower()]
+
     def filterByName(self, name):
-        if self.data is None:
-            return None
+        return self._filter_by_field("name", name)
 
-        returnable_data = []
-        name = name.lower()
-        for data in self.data:
-            item_name = data.get("name", "")
-
-            if name in item_name.lower():
-                returnable_data.append(data)
-
-        return returnable_data
-
-    def filterBySet(self, set):
-        if self.data is None:
-            return None
-
-        returnable_data = []
-        set = set.lower()
-        for data in self.data:
-            item_set = data.get("set", "")
-
-            if set in item_set.lower():
-                returnable_data.append(data)
-
-        return returnable_data
+    def filterBySet(self, set_name):
+        return self._filter_by_field("set", set_name)
 
     def filterByInfo(self, info):
-        if self.data is None:
-            return None
-
-        returnable_data = []
-        info = info.lower()
-        for data in self.data:
-            item_info = data.get("info", "")
-
-            if set in item_info.lower():
-                returnable_data.append(data)
-
-        return returnable_data
+        return self._filter_by_field("info", info)
 
     def filterByColor(self, color):
-        if self.data is None:
-            return None
+        return self._filter_by_field("color", color)
 
-        returnable_data = []
-        color = color.lower()
-        for data in self.data:
-            item_color = data.get("color", "")
-
-            if color in item_color.lower():
-                returnable_data.append(data)
-
-        return returnable_data
+    def getData(self):
+        return self.data
--- server.py
+++ server.py
@@ -1,39 +1,50 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 from jsonParse import DataFetcher
 import json
 
 app = Flask(__name__)
+fetcher = DataFetcher()
+
+
+@app.before_request
+def initialize():
+    fetcher.parse('data.json')
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/fetchAll')
 def fetchall():
-    fetcher.parse('data.json')
-    results = fetcher.filterByName("")
-    return results
+    results = fetcher.getData()
+    return jsonify(results)
 
-@app.route('/fetchMatchesByName/<string:name>')
+
+@app.route('/name/<string:name>')
 def fetchMatchesByName(name: str):
-    fetcher.parse('data.json')
     results = fetcher.filterByName(name)
-    return results
+    return jsonify(results)
 
-@app.route('/fetchMatchesBySet/<string:set>')
+
+@app.route('/set/<string:set>')
 def fetchMatchesBySet(set: str):
-    fetcher.parse('data.json')
     results = fetcher.filterBySet(set)
-    return results
+    return jsonify(results)
 
-@app.route('/fetchMatchesByColor/<string:color>')
+
+@app.route('/color/<string:color>')
 def fetchMatchesByColor(color: str):
-    fetcher.parse('data.json')
     results = fetcher.filterByColor(color)
-    return results
+    return jsonify(results)
 
-@app.route('/fetchMatchesByInfo/<string:info>')
+
+@app.route('/info/<string:info>')
 def fetchMatchesByInfo(info: str):
-    fetcher.parse('data.json')
     results = fetcher.filterByInfo(info)
-    return results
+    return jsonify(results)
+
 
 if __name__ == "__main__":
-    fetcher = DataFetcher()
+    fetcher.parse('data.json')
     app.run(debug=True)
